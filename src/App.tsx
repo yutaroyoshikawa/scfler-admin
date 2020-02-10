05d27f268aaf51ecea20fb55c24ad24b70ae311d
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-10 12:00:45 +0900

    eslintrcインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+interface PageItem {
+  url: string;
+  component: React.FC<any>;
+}
+
+const pages: PageItem[] = [
+  {
+    url: "",
+    component: Home
+  }
+];
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        {pages.map(page => (
+          <Route>
+            <page.component />
+          </Route>
+        ))}
+      </Switch>
+    </Router>
   );
 };
 
