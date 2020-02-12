5422bb95fa4a528513ad9ff77ef66735f725517b
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-12 17:25:22 +0900

    ログインフォームスタイル修正

--- src/components/LoginProvider.tsx
+++ src/components/LoginProvider.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { makeStyles } from "@material-ui/styles";
+import { Theme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import Title from "./Title";
 
-const useStyle = makeStyles(theme => ({
+const useStyle = makeStyles((theme: Theme) => ({
   wrap: {
     width: "100vw",
     height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  formWrap: {
+    maxWidth: "500px",
+    width: "100%"
+  },
+  textFieldWrap: {
+    margin: `${theme.spacing(2)}px 0`
+  },
+  textField: {
+    width: "100%"
   }
 }));
 
@@ -22,9 +39,12 @@ const GET_LOGIN_STATE = gql`
 
 const LoginProvider: React.FC = props => {
   const { data, client } = useQuery(GET_LOGIN_STATE);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const classes = useStyle();
 
-  const onLogin = () => {
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     client.writeData({
       data: {
         isLoggedIn: true
@@ -36,10 +56,34 @@ const LoginProvider: React.FC = props => {
     <>
       {!data.isLoggedIn ? (
         <div className={classes.wrap}>
-          <h1>ログインしてください。</h1>
-          <Button variant="contained" color="primary" onClick={() => onLogin()}>
-            ログイン
-          </Button>
+          <form className={classes.formWrap} onSubmit={e => onSubmit(e)}>
+            <Card className={classes.formWrap}>
+              <CardContent>
+                <Title>管理画面にログイン</Title>
+                <div className={classes.textFieldWrap}>
+                  <InputLabel>Email</InputLabel>
+                  <TextField
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    className={classes.textField}
+                  />
+                </div>
+                <div className={classes.textFieldWrap}>
+                  <InputLabel>パスワード</InputLabel>
+                  <TextField
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className={classes.textField}
+                  />
+                </div>
+              </CardContent>
+              <CardActions>
+                <Button type="submit" size="large">
+                  ログイン
+                </Button>
+              </CardActions>
+            </Card>
+          </form>
         </div>
       ) : (
         <>{props.children}</>
